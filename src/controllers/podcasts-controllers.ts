d953cd33266b748d57646ddb6a7ff497c253f76c
Evanio Tech <evanioshark@gmail.com>
2025-02-26 10:09:16 -0300

    route of filter created

--- src/controllers/podcasts-controllers.ts
+++ src/controllers/podcasts-controllers.ts
@@ -1,9 +1,20 @@
 import { IncomingMessage, ServerResponse } from "http";
 
+import { serviceFilterEp } from "../services/filter-ep-service";
+
 import { ServiceListEpisodes } from "../services/list-episodes-service";
+import { StatusCode } from "../utils/status-code";
 
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(StatusCode.OK, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(await ServiceListEpisodes()));
+}
+
+
+export const getFilstEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+
+ 
+  res.writeHead(StatusCode.OK, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(await serviceFilterEp(req.url)));
 }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
 import * as http from 'http';
 
-import { getListEpisodes } from './controllers/podcasts-controllers';
+import { getFilstEpisodes, getListEpisodes } from './controllers/podcasts-controllers';
+import { Routes } from './routes';
+import { HttpMethod } from './utils/http-methods';
 
 const server = http.createServer( async (req: http.IncomingMessage, res: http.ServerResponse) => {
-  if (req.method === "GET") {
+
+  //query string
+  const [baseURL, queryString] = req.url?.split('?') ?? ["", ""];
+  console.log(baseURL);
+  console.log(queryString);
+ 
+  
+  if (req.method === HttpMethod.GET && baseURL === Routes.LIST) {
     await getListEpisodes(req, res);
+  }
+  if (req.method === HttpMethod.GET && baseURL === Routes.EPISODE) {
+    await getFilstEpisodes(req, res);
   }
 }
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3636;
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
